8138b3cd414f7380dbd143ec022414ce7b1fa428
Akki19hzb <getakash.verma19@gmail.com>
2019-07-14 18:04:08 +0530

    add and delete functionality are added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
-
+import AddNinja from './AddNinja'
 class App extends Component {
   state = {
     ninjas: [
@@ -9,11 +9,34 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+
+  addNinja= (ninja) => {
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas : ninjas
+
+    });
+    console.log(this.state.ninjas);
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja =>{
+      return ninja.id !== id
+    })
+    this.setState(
+      {
+        ninjas : ninjas
+      }
+    )
+    console.log(ninjas)
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja = {this.deleteNinja}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
